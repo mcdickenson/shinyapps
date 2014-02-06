211faff196bf4ca4b99b6932742ea9d51ea810e4
Matt Dickenson <mcdickenson@gmail.com>
2014-02-06 11:55:57 -0500

    add ibp example

---
+++ ibp/ui.R
@@ -0,0 +1,24 @@
+library(shiny)
+
+shinyUI(pageWithSidebar(
+
+  headerPanel("Indian Buffet Process"),
+
+  sidebarPanel(
+     sliderInput("alpha",
+        "Average number of dishes (alpha):",
+        value=10,
+        min=1,
+        max=20,
+        step=1
+      ),
+
+     numericInput("n", "Number of customers (N):", 10),
+
+     submitButton("Update")
+    ),
+
+  mainPanel(
+    plotOutput("ibpPlot")
+  )
+))
